537d6c5a8199c10c7070b321876a6f35d4961a46
Franco Goslino <125417699+FrancoGoslino@users.noreply.github.com>
2025-07-12 21:12:33 -0300

    FUNCTION -> Integración de firebase en registro. Se cambia a un EMULADOR API 36; ABI x86_64; Google Play y Apis

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -48,4 +48,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/testapplication/RegisterUser.java
+++ app/src/main/java/com/example/testapplication/RegisterUser.java
@@ -1,21 +1,37 @@
 package com.example.testapplication;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterUser extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_user);
@@ -27,6 +43,45 @@ public class RegisterUser extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+    public void ResgitroApp(View view) {
+
+
+        EditText emailEditText = findViewById(R.id.editTextEmailRegister);
+        EditText passwordEditText = findViewById(R.id.editTextPasswordRegister);
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Correo inválido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Registro del usuario  Firebase
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(this, MainActivity2.class));
+                    } else {
+                        Toast.makeText(this, "Error al registrar: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
     public void OnClick_VolverAtras(View view){
         Intent intent = new Intent(this, MainActivity.class);
