fdd5e802a5efa89e5f122469f333b9eb042c40cb
Franco Goslino <125417699+FrancoGoslino@users.noreply.github.com>
2025-07-13 12:46:59 -0300

    FUNCTION -> Agrega Nombre de usuario al Registro, Rediseño de su pantalla con nuevos elementos. Se agregan implementaciones e importaciones de HASHMAP.

--- app/src/main/java/com/example/testapplication/RegisterUser.java
+++ app/src/main/java/com/example/testapplication/RegisterUser.java
@@ -2,6 +2,8 @@ package com.example.testapplication;
 
 import static android.content.ContentValues.TAG;
 
+import static java.util.regex.Pattern.matches;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,6 +25,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterUser extends AppCompatActivity {
 
@@ -52,34 +59,58 @@ public class RegisterUser extends AppCompatActivity {
             currentUser.reload();
         }
     }
-    public void ResgitroApp(View view) {
 
+    public void ResgitroApp(View view) {
+        EditText UserEditText = findViewById(R.id.editTextUserRegister);
         EditText emailEditText = findViewById(R.id.editTextEmailRegister);
         EditText passwordEditText = findViewById(R.id.editTextPasswordRegister);
 
+        String user1 = UserEditText.getText().toString().trim();
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
 
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            Toast.makeText(this, "Correo inválido", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        // Validaciónes
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "Correo inválido", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (password.length() < 6 && !password.matches(".*[A-Z].*") && !password.matches(".*[a-z].*")) {
+                Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres, una letra mayúscula y una letra minúscula", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!user1.matches("^[a-zA-Z]{4,}$")) {
+                Toast.makeText(this, "El nombre de usuario debe tener al menos 4 letras sin números ni símbolos", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (user1.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
         // Registro del usuario  Firebase
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         FirebaseUser user = mAuth.getCurrentUser();
+                        String uid = user.getUid();
+
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                        Map<String, Object> datosUsuario = new HashMap<>();
+                        datosUsuario.put("nombreUsuario", user1);
+                        datosUsuario.put("email", email);
+                        datosUsuario.put("fechaRegistro", new Date());
+
+                        db.collection("Usuarios").document(uid).set(datosUsuario)
+                                .addOnSuccessListener(aVoid -> Log.d("Firestore", "Usuario guardado"))
+                                .addOnFailureListener(e -> Log.w("Firestore", "Error al guardar", e));
+
                         Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(this, MainActivity2.class));
-                    } else {
-                        Toast.makeText(this, "Error al registrar: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+
     }
 
     public void OnClick_VolverAtras(View view){
