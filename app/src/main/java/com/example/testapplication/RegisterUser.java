3729ca28b633b41fc29b25e1e6a9d3538412d962
Franco Goslino <125417699+FrancoGoslino@users.noreply.github.com>
2025-07-13 16:37:44 -0300

    DISEÑO -> Re diseño pantallas: activity_main.xml, activity_register_user.xml, fragment_add_task.xml

--- app/src/main/java/com/example/testapplication/RegisterUser.java
+++ app/src/main/java/com/example/testapplication/RegisterUser.java
@@ -43,7 +43,7 @@ public class RegisterUser extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_user);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resgisterUser), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registerUser), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
